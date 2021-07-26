605685c11ab9cc1deb4b2172bea4cbb8ef346c85
killercosmos <76935007+killercosmos@users.noreply.github.com>
2021-07-26 19:45:23 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-function addusser() {
-    var player1_name = document.getElementById("player_1_input").value;
-    var player2_name = document.getElementById("player_2_input").value;
-    localStorage.setItem("player1_name" , player1_name);
-    localStorage.setItem("player2_name" , player2_name);
-    window.location = "gamepage.html";
-}
